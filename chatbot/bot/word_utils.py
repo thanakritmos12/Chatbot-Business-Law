856c965d9ded3315c3bb5eb5f17307d359c72d4e
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-03-17 16:30:05 +0700

    cleaning file by removing unnecessary comment

--- chatbot/bot/word_utils.py
+++ chatbot/bot/word_utils.py
@@ -3,25 +3,16 @@ import deepcut
 # from pythainlp.tokenize import sent_tokenize
 def clean_words(words):
     stopwords = pythainlp.corpus.common.thai_stopwords()
-    # stopwords = ['', ' ', ',', '.', 'หรือไม่', 'มั้ย', 'ไหม', 'ยังไง', 'ยัง', 'ไง', 'ใหม', 'มัํย', 'มั่ย', 'บ้าง', 'นะ', 'หนา', 'บ้างไม', 'ไม', 'ๆ', 'ปะ', 'ครับ', 'คับ', 'ค่ะ', 'คะ', 'ค้ะ', 'ค้า', 'ค้าบ']
     data = []
     for word in words:
         word = word.strip()
-
         if word not in stopwords:
             data.append(word)
-    # data = []
-    # for word in words:
-    #     data = word.strip()
     return data
 
 
 def get_features(data):
     words = deepcut.tokenize(data)
-    # words = pythainlp.word_tokenize(data, engine='longest')
-    # words = pythainlp.word_tokenize(data, engine='deepcut')
-    # words = sent_tokenize(data)
-    # words = pythainlp.syllable_tokenize(data)
     words = clean_words(words)
     features = {}
     features['กฎหมาย'] = 'กฎหมาย' in words
