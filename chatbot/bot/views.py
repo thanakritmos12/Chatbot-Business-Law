0de51585e0edbb136056b60ff9ee36687d5e23b1
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-03-17 11:29:53 +0700

    update response and views's method

--- chatbot/bot/views.py
+++ chatbot/bot/views.py
@@ -4,9 +4,9 @@ import requests, random, re
 import pprint
 import random
 import pandas
-from chatbot.chatbot import word_utils
+# from chatbot.chatbot import word_utils
 import pickle
-
+import os
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
@@ -19,8 +19,30 @@ VERIFY_TOKEN = "c735ab9888f151a3721996bef579848694c922b44628dfe489" # generated 
 FB_ENDPOINT = 'https://graph.facebook.com/v12.0/'
 PAGE_ACCESS_TOKEN = "EAAE3KGhIo6EBAKok7UJZAAA45D0aqgRxrCvZBeZBQZBnhHrOSy9MqrlW317cPYTJSJzGtkhGfTDvEJX4hrrgS32xSbvhznEV3Irez1xBpwYvdtZBjvkZBaho1Je63NGtCg23fGQwE7Uy2x9wAMZBqzLlVo1xIAqzMMJ9GOQJ5ZCEUkI6XZC0s4gPX"  
 
+import pythainlp
+
+def clean_words(words):
+    # stopwords = pythainlp.corpus.common.thai_stopwords()
+    # stopwords = ['', ' ', ',', '.', 'หรือไม่', 'มั้ย', 'ไหม', 'ยังไง', 'ยัง', 'ไง', 'ใหม', 'มัํย', 'มั่ย', 'บ้าง', 'นะ', 'หนา', 'บ้างไม', 'ไม']
+    data = []
+    for word in words:
+        data = word.strip()
+    return data
+
+
+def get_features(data):
+    # words = pythainlp.word_tokenize(data, engine='multi_cut')
+    words = pythainlp.word_tokenize(data, engine='deepcut')
+    # words = pythainlp.syllable_tokenize(data)
+    words = clean_words(words)
+    data = {"words": " ".join(words), "count": len(words)}
+    return data
+
 def parse_and_send_fb_message(fbid, recevied_message):
-    dfs = pandas.read_excel("response.xlsx")
+    # Remove all punctuations, lower case the text and split it based on space
+    # tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+
+    dfs = pandas.read_excel(r'C:\Users\user\Projects\baisd\chat_bot_team\chatbot\bot\response.xlsx')
 
     responses = {}
     for df in dfs.to_dict("records"):
@@ -28,15 +50,29 @@ def parse_and_send_fb_message(fbid, recevied_message):
             responses[df["tag"]] = []
         responses[df["tag"]].append(df["response"])
 
-    f = open("my_classifier.pickle", "rb")
+    f = open(r"C:\Users\user\Projects\baisd\chat_bot_team\chatbot\bot\my_classifier.pickle", "rb")
     classifier = pickle.load(f)
     f.close()
 
-    message_fb = recevied_message.lower().split()
-    feature = word_utils.get_features(message_fb)
+    message_fb = recevied_message
+    feature = get_features(message_fb)
     result = classifier.prob_classify(feature)
+    if result.prob(result.max()) < 0.4:
 
-    msg = random.choice(responses[result.max()])
+        msg = """ ไม่เข้าจายย พิมพ์ตามหัวข้อนี้หน่อยน้า
+        1. กฎหมายที่เกี่ยวข้องกับการขายของออนไลน์มีกฎหมายสำคัญๆใดที่เกี่ยวข้องบ้าง
+        2. การแชร์รูปภาพหรือข้อความต่างๆ เกี่ยวกับร้านค้าออนไลน์ของตนในพื้นที่โซเซียลมีดีของผู้อื่นผิดกฎหมายหรือไม่
+        3. ขั้นตอนการจดทะเบียนพาณิชย์ในการค้าขายออนไลน์มีอะไรบ้าง
+        4. การเปิดร้าน
+        5. กฎหมายเกี่ยวกับการขายของออนไลน์มีไว้ทำไม
+        6. กฎหมายขายของออนไลน์เกี่ยวข้องอย่างไรกับกฎหมายคุ้มครองผู้บริโภค
+        7. สินค้าที่ห้ามขายออนไลน์
+        8. ร้านค้าออนไลน์ควรแสดงรายละเอียด
+        9. ร้านค้าออนไลน์ต้องเสียภาษีอย่างไร
+        10. ร้านค้าออนไลน์จะขายตรงและตลาดแบบตรงได้หรือไม่
+        """
+    else :
+        msg = random.choice(responses[result.max()])
         
     if msg is not None:                 
         endpoint = f"{FB_ENDPOINT}/me/messages?access_token={PAGE_ACCESS_TOKEN}"
@@ -48,6 +84,7 @@ def parse_and_send_fb_message(fbid, recevied_message):
         print(status.json())
         return status.json()
     return None
+
         
         
 class FacebookWebhookView(View):
