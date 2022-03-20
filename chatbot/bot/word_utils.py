5261228c42a83a503add54df2f7ccb0bdca53334
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-03-20 16:02:25 +0700

    change features and change stopwords

--- chatbot/bot/word_utils.py
+++ chatbot/bot/word_utils.py
@@ -4,7 +4,9 @@ import deepcut
 
 
 def clean_words(words):
-    stopwords = pythainlp.corpus.common.thai_stopwords()
+    # stopwords = pythainlp.corpus.common.thai_stopwords()
+    stopwords = ['กฎหมาย', 'กฏหมาย', 'อะไร', 'บ้าง', 'อะไรบ้าง', 'ไหม', 'มั้ย', 'ไม', 'ไร', 'ร้านค้า', 'ร้าน',
+     'ค้า', 'กฎ', 'การ', 'ความ', 'ใน', 'ที่', 'ต่างๆ', 'ได้']
     data = []
     for word in words:
         word = word.strip()
@@ -18,35 +20,38 @@ def get_features(data):
     words = clean_words(words)
     features = {}
     # ข้อ 1
-    features['กฎหมาย'] = 'กฎหมาย' in words or 'สำคัญ' in words or 'ต้อง' in words or 'รู้' in words or 'กฎ' in words
-    features['ร้านค้า'] = 'ร้านค้า' in words or 'ออนไลน์' in words or 'online' in words or 'ร้าน' in words or 'ค้า' in words
+    features['ข้อหนึ่ง'] = 'สำคัญ' in words or 'สำคัญๆ' in words or 'ควรรู้' in words or 'ควร' in words or 'รู้' in words or 'ต้องรู้' in words or 'ต้อง' in words or 'ทราบ' in words or 'ควรทราบ' in words or 'เกี่ยวข้อง' in words or 'ออนไลน์' in words or 'online' in words
 
     # ข้อ 2
-    features['แชร์'] = 'แชร์รูปภาพ' in words or 'แชร์ภาพ' in words or 'แชร์ข้อความ' in words or 'สาธารณะ' in words or 'ฝากร้าน' in words or 'โปรโมท' in words or 'โพส' in words
+    features['ข้อสอง'] = 'แชร์รูปภาพ' in words or 'แชร์ภาพ' in words or 'ข้อความ' in words or 'สาธารณะ' in words or 'ฝากร้าน' in words or 'โปรโมท' in words or 'โพส' in words or 'แชร์' in words or 'แชร์โพสต์' in words or 'แชร์โพส' in words or 'แช' in words or 'share' in words or 'post' in words or 'link' in words or 'ภาพ' in words or 'กลุ่ม' in words or 'แนะนำ' in words or 'ฝาก' in words or 'แชร์ร้าน' in words or 'แชร้าน' in words
 
-    # ข้อ 3
-    features['พานิชย์'] = 'พานิชย์' in words or 'จดทะเบียนพานิชย์' in words or 'ทะเบียนพานิชย์' in words or 'ทะเบียนการค้า' in words or 'ทะเบียนค้าขาย' in words or 'ทะเบียนซื้อขาย' in words
-    # ข้อ 4
-    features['เริ่ม'] = 'เริ่ม' in words or 'ขั้นแรก' in words or 'ก่อนขาย' in words or 'เตรียม' in words or 'สร้าง' in words or 'เรื่มต้น' in words
+    # # ข้อ 3
+    # features['พานิชย์'] = 'พานิชย์' in words or 'จดทะเบียนพานิชย์' in words or 'ทะเบียนพานิชย์' in words or 'ทะเบียนการค้า' in words or 'ทะเบียนค้าขาย' in words or 'ทะเบียนซื้อขาย' in words
+    # # ข้อ 4
+    # features['เริ่ม'] = 'เริ่ม' in words or 'ขั้นแรก' in words or 'ก่อนขาย' in words or 'เตรียม' in words or 'สร้าง' in words or 'เรื่มต้น' in words
 
-    # ข้อ 5
-    features['อิเล็กทรอนิกส์'] = 'อิเล็กทรอนิกซ์' in words or 'อิเล็กทรอนิก' in words or 'อิเล็กทรอนิกซ์' in words or 'DBD' in words or 'dbd ' in words or 'registered' in words or 'Registered' in words
+    # # ข้อ 5
+    # features['อิเล็กทรอนิกส์'] = 'อิเล็กทรอนิกซ์' in words or 'อิเล็กทรอนิก' in words or 'อิเล็กทรอนิกซ์' in words or 'DBD' in words or 'dbd ' in words or 'registered' in words or 'Registered' in words
 
-    # ข้อ 6 
-    features['คุ้มครอง'] = 'คุ้มครอง' in words or  'กฎหมายคุ้มครอง' in words or 'ผู้บริโภค' in words or 'สคบ.' in words or 'สตบ.' in words
+    # # ข้อ 6 
+    # features['คุ้มครอง'] = 'คุ้มครอง' in words or  'กฎหมายคุ้มครอง' in words or 'ผู้บริโภค' in words or 'สคบ.' in words or 'สตบ.' in words
 
-    # ข้อ 7
-    features['ห้าม'] = 'ห้าม' in words or 'ผิด' in words or 'ลักลอบ' in words or 'อันตราย' in words or 'ต้องห้าม' in words
+    # # ข้อ 7
+    # features['ห้าม'] = 'ห้าม' in words or 'ผิด' in words or 'ลักลอบ' in words or 'อันตราย' in words or 'ต้องห้าม' in words
 
-    # ข้อ 8
-    features['รายละเอียด'] = 'รายละเอียด' in words or 'ข้อมูล' in words or 'แสดง' in words or 'โชว์' in words or 'ประวัติ' in words
+    # # ข้อ 8
+    # features['รายละเอียด'] = 'รายละเอียด' in words or 'ข้อมูล' in words or 'แสดง' in words or 'โชว์' in words or 'ประวัติ' in words
 
-    # ข้อ 9
-    features['ภาษี'] = 'ภาษี' in words or 'จ่าย' in words or 'โดน' in words
+    # # ข้อ 9
+    # features['ภาษี'] = 'ภาษี' in words or 'จ่าย' in words or 'โดน' in words
 
-    # ข้อ 10
-    features['ขาย'] = 'ขาย' in words or 'ขายตรง' in words or 'ตรง' in words
-    features['ตลาด'] = 'ตลาด' in words or 'ตรง' in words or 'ตลาดตรง' in words
+    # # ข้อ 10
+    # features['ขาย'] = 'ขาย' in words or 'ขายตรง' in words or 'ตรง' in words
+    # features['ตลาด'] = 'ตลาด' in words or 'ตรง' in words or 'ตลาดตรง' in words
+
+    # ทักทาย
+    # features['Hi'] = 'Hi' in words or 'Hello' in words or 'Yea' in words or 'Sup' in words or 'Yo' in words or 'Hey' in words or 'up' in words
+    # features['ดี'] = 'ดี' in words or 'สวัสดี' in words or 'หวัดดี' in words or 'ดีจ้า' in words or 'ค้า' in words or 'ฮัลโหล' in words or 'โหล' in words or 'โย่' in words or 'ไง' in words or 'โหล' in words
     data = {"words": " ".join(words), "count": len(words)}
     data.update(features)
     return data
