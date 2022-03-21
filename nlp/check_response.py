0caae294f2ffb4e97e8809a86d7eff18161f198d
Pathitta Suksomboon <praewiizzpraew@gmail.com>
2022-03-21 17:47:16 +0700

    change path now check_respnse is working

--- nlp/check_response.py
+++ nlp/check_response.py
@@ -2,10 +2,19 @@ import nltk
 import pprint
 import random
 import pandas
-import word_utils
+import pathlib
 import pickle
+import sys
 
-dfs = pandas.read_excel("response1.xlsx")
+sys.path.append('.')
+
+from chatbot.bot import word_utils
+
+
+path = pathlib.Path(__file__).parent.parent
+
+    # prepare data
+dfs = pandas.read_excel(path/"data/response.xlsx")
 
 responses = {}
 for df in dfs.to_dict("records"):
@@ -15,7 +24,7 @@ for df in dfs.to_dict("records"):
 
 pprint.pprint(responses)
 ## To load later:
-f = open("my_classifier.pickle", "rb")
+f = open("data/my_classifier.pickle", "rb")
 classifier = pickle.load(f)
 f.close()
 
@@ -27,7 +36,7 @@ while True:
     print(feature)
     result = classifier.prob_classify(feature)
     print("prob ->", result.max(), round(result.prob(result.max()), 2))
-    if result.prob(result.max()) < 0.15:
+    if result.prob(result.max()) < 0.55:
         print('หาไม่เจอ')
         continue
 
