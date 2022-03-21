04f22355dd2bd3bcc1da705d8293c159ae73832b
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-03-21 19:24:02 +0700

    add more keywords for greeting tag

--- chatbot/bot/word_utils.py
+++ chatbot/bot/word_utils.py
@@ -51,7 +51,7 @@ def get_features(data):
 
     # ทักทาย
     features['greet'] = 'Hi' in words or 'Hello' in words or 'Yeah' in words or 'Sup' in words or 'Yo' in words or 'Hey' in words or 'up' in words or 'Yea' in words or 'Yess' in words or 'Yes' in words or 'Yep' in words or 'What' in words
-    features['ทักทาย'] = 'ดี' in words or 'สวัสดี' in words or 'หวัดดี' in words or 'ดีจ้า' in words or 'ค้า' in words or 'ฮัลโหล' in words or 'โหล' in words or 'โย่' in words or 'ไง' in words or 'โหล' in words
+    features['ทักทาย'] = 'ดี' in words or 'สวัสดี' in words or 'หวัดดี' in words or 'ดีจ้า' in words or 'ค้า' in words or 'ฮัลโหล' in words or 'โหล' in words or 'โย่' in words or 'ไง' in words or 'โหล' in words or 'ดี' in words 
     data = {"words": " ".join(words), "count": len(words)}
     data.update(features)
     return data
