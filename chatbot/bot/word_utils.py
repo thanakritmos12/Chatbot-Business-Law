cfa9591c01c27c3d723ca85523798788ae2b867f
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-03-22 02:24:17 +0700

    remove and add some keywords for each question

--- chatbot/bot/word_utils.py
+++ chatbot/bot/word_utils.py
@@ -5,8 +5,9 @@ import deepcut
 
 def clean_words(words):
     # stopwords = pythainlp.corpus.common.thai_stopwords()
-    stopwords = ['กฎหมาย','กฏหมาย','อะไร', 'บ้าง', 'อะไรบ้าง', 'ไหม', 'มั้ย', 'ไม', 'ไร', 'ร้านค้า', 'ร้าน',
-     'ค้า', 'กฎ', 'การ', 'ความ', 'ใน', 'ที่', 'ต่างๆ', 'ได้', 'อย่าง','ออนไลน์','online','ขาย' ,'ไง', 'อะ', 'อ้ะ', 'หรอ']
+    stopwords = ['กฎหมาย','กฏหมาย','บ้าง', 'ไหม', 'มั้ย', 'ไม', 'ร้านค้า', 'ร้าน',
+     'ค้า', 'กฎ', 'การ', 'ความ', 'ใน', 'ที่', 'ต่างๆ', 'ได้', 'อย่าง','ออนไลน์','online','ขาย' ,
+     'ไง','อ้ะ', 'หรอ', 'อะไร', 'อยู่', 'อยาก', 'ทราบ', 'ของ']
     data = []
     for word in words:
         word = word.strip()
@@ -26,10 +27,10 @@ def get_features(data):
     features['ข้อสอง'] = 'แชร์รูปภาพ' in words or 'แชร์ภาพ' in words or 'ข้อความ' in words or 'สาธารณะ' in words or 'ฝากร้าน' in words or 'โปรโมท' in words or 'โพส' in words or 'แชร์' in words or 'แชร์โพสต์' in words or 'แชร์โพส' in words or 'แช' in words or 'share' in words or 'post' in words or 'link' in words or 'ภาพ' in words or 'กลุ่ม' in words or 'แนะนำ' in words or 'ฝาก' in words or 'แชร์ร้าน' in words or 'แชร้าน' in words or 'โฆษณา' in words or '2' in words
 
     # ข้อ 3
-    features['ข้อสาม'] = 'พานิชย์' in words or 'จดทะเบียนพานิชย์' in words or 'ทะเบียนพานิชย์' in words or 'ทะเบียนการค้า' in words or 'ทะเบียนค้าขาย' in words or 'ทะเบียนซื้อขาย' in words or 'ขั้นตอน' in words or 'จด' in words or 'ทะเบียน' in words or 'พาณิชย์' in words or 'พาณิช' in words or 'เบียน' in words or 'จดทะเบียน' in words or '3' in words
+    features['ข้อสาม'] = 'พานิชย์' in words or 'จดทะเบียนพานิชย์' in words or 'ทะเบียนพานิชย์' in words or 'ทะเบียนการค้า' in words or 'ทะเบียนค้าขาย' in words or 'ทะเบียนซื้อขาย' in words or 'ขั้นตอน' in words or 'จด' in words or 'ทะเบียน' in words or 'พาณิชย์' in words or 'พาณิช' in words or 'เบียน' in words or 'จดทะเบียน' in words or '3' in words or 'วิธี' in words
 
     # ข้อ 4
-    features['ข้อสี่'] = 'เริ่ม' in words or 'ขั้นแรก' in words or 'ก่อนขาย' in words or 'เตรียม' in words or 'สร้าง' in words or 'เริ่มต้น' in words or 'ก่อน' in words or 'เปิด' in words or 'เริ่ม' in words or 'สร้าง' in words or 'แรก' in words or 'อยาก' in words or '4' in words
+    features['ข้อสี่'] = 'เริ่ม' in words or 'ขั้นแรก' in words or 'ก่อนขาย' in words or 'เตรียม' in words or 'สร้าง' in words or 'เริ่มต้น' in words or 'ก่อน' in words or 'เปิด' in words or 'สร้าง' in words or 'แรก' in words or 'อยาก' in words or '4' in words
 
     # ข้อ 5
     features['ข้อห้า'] = 'อิเล็กทรอนิกซ์' in words or 'อิเล็กทรอนิก' in words or 'อิเล็กทรอนิกส์' in words or 'DBD' in words or 'dbd ' in words or 'registered' in words or 'Registered' in words or 'แบบ' in words or 'ลักษณะ' in words or 'ประ' in words or 'เภท' in words or 'ประเภท' in words or 'shopee' in words or 'lazada' in words or 'electronic' in words or 'electronics' in words or 'Electronic' in words or 'Electronics' in words or '5' in words
@@ -50,7 +51,7 @@ def get_features(data):
     features['ข้อสิบ'] = 'ขาย' in words or 'ขายตรง' in words or 'ตรง' in words or 'ตลาด' in words or 'ตลาดตรง' in words or '10' in words
 
     # ทักทาย
-    features['ทักทาย'] = 'ดี' in words or 'สวัสดี' in words or 'หวัดดี' in words or 'ดีจ้า' in words or 'ค้า' in words or 'ฮัลโหล' in words or 'โหล' in words or 'โย่' in words or 'ไง' in words or 'โหล' in words or 'ดี' in words or 'Hi' in words or 'Hello' in words or 'Yeah' in words or 'Sup' in words or 'Yo' in words or 'Hey' in words or 'up' in words or 'Yea' in words or 'Yess' in words or 'Yes' in words or 'Yep' in words or 'What' in words
+    features['ทักทาย'] = 'ดี' in words or 'สวัสดี' in words or 'หวัดดี' in words or 'ดีจ้า' in words or 'ค้า' in words or 'ฮัลโหล' in words or 'โหล' in words or 'โย่' in words or 'ไง' in words or 'โหล' in words or 'ดี' in words or 'Hi' in words or 'Hello' in words or 'Yeah' in words or 'Sup' in words or 'Yo' in words or 'Hey' in words or 'up' in words or 'Yea' in words or 'Yess' in words or 'Yes' in words or 'Yep' in words or 'What' in words or 'bot' in words or 'ดีบอท' in words or 'บอท' in words
 
     # สบายดี
     features['สบายดี'] = 'สบาย' in words or 'บาย' in words or 'บัย' in words or 'สบายดี' in words
@@ -62,13 +63,11 @@ def get_features(data):
     features['ชมบอท'] = 'เก่งมาก' in words or 'เก่ง' in words
 
     # ทำอะไร
-    features['ทำอะไร'] = 'ทำอะไร' in words or 'ทำ' in words or 'ตอบ' in words or 'งง' in words or 'ตรง' in words
+    features['ทำอะไร'] = 'ทำอะไร' in words or 'ทำ' in words
 
     # ชื่ออะไร
     features['ชื่ออะไร'] = 'ชื่อ' in words or 'ชื่ออะไร' in words
 
-
-
     data = {"words": " ".join(words), "count": len(words)}
     data.update(features)
-    return data
+    return data 
